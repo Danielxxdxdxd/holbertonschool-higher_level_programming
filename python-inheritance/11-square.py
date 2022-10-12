4f2b153d353e9814fe12f7c088e5a71763b69847
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-10-12 10:44:58 -0500

    Square #2

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,22 +1,49 @@
 #!/usr/bin/python3
+"""
+Module Square class with
+inherits Rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-"""This module creates a named square"""
 
-Rectangle = __import__('9-rectangle').Rectangle
+class Rectangle(BaseGeometry):
+    """Initization Rectangle"""
+
+    def __init__(self, width, height):
+        """
+        width and height is a private instance
+        Validation width and height must be
+        positive integers
+        """
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Return area for a object
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """
+        Returns size for a object with a message
+        """
+        size_msg = ("[{}] {}/{}".format(self.__class__.__name__,
+                    self.__width, self.__height))
+        return (size_msg)
 
 
 class Square(Rectangle):
-    """An class named BaseGeometry
-    Attributes:
-    attr1(area): Raises an exception
-    """
+    """Initization Rectangle"""
 
     def __init__(self, size):
-        """Initializes an instance"""
-        self.integer_validator("size", size)
+        """
+        Size is a private instance
+        Validation size must be
+        positive integers
+        """
+        Rectangle.integer_validator(self, "size", size)
         self.__size = size
         super().__init__(size, size)
-
-    def __str__(self):
-        """Return a string for the area"""
-        return f"[Square] {self.__size}/{self.__size}"
