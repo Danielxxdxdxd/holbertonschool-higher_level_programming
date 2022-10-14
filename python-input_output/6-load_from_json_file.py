fae3ebaae91b5de8753c810f9ac1a81d65d14a5b
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-10-13 19:00:27 -0500

    Create object from a JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
-"""module with function to save to json file"""
-
+"""
+Module 6-load_from_json_file with load_from_json_file(filename)
+Create an object form a JSON file
+"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """save to json file
-    Args:
-    my_obj: obj to save
-    filename: file to save to
+def load_from_json_file(filename):
     """
-
-    with open(filename, 'w', encoding='utf-8') as file:
-        file.write(json.dumps(my_obj))
+    Create a object with json format
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
