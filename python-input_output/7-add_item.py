9c44f3bd2f0dbc49557884aeb1321fcb1fc84162
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-10-13 19:01:33 -0500

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""
+Module 7-add_item
+Use with two lasted modules
+"""
+import sys
 
-"""Adds python args to a list and save them"""
-
-from os import path
-from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__(
-    '6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json") is False:
-    save_to_json_file([], "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+open("add_item.json", "a")
+try:
+    file = load_from_json_file("add_item.json")
+except (ValueError):
+    file = []
+save_to_json_file(file + sys.argv[1:], "add_item.json")
