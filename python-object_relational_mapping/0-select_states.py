e38dc2621c179f79e9a536e84cd217dce6bb0d72
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-11-18 16:04:59 -0500

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-"""Conect the database"""
+"""lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
-import sys
-
-
-def mysqlconnect():
-    db_connection = None
-    db_connection = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-
-    cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM states")
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db_connection.close()
-
+from sys import argv
 
 if __name__ == '__main__':
-    mysqlconnect()
+
+    """
+    Create a connection to the database
+    arguments:
+        1. server name
+        2. database user name
+        3. database password
+        4. database name
+    """
+    data_base = MySQLdb.connect(host='localhost', user=argv[1],
+                                password=argv[2], database=argv[3])
+
+    """
+    Creates a cursor that is capable of
+    executing SQL queries on the database
+    """
+    cursor = data_base.cursor()
+
+    """
+    Executing SQL queries on the database
+    Takes a SQL query as a string, as an argument
+    """
+    cursor.execute('SELECT * FROM states')
+
+    """
+    Fetch (extraer) the rows of the table and store them in a variable
+    The data is store as a Tuple of tuples
+    """
+    data_rows = cursor.fetchall()
+
+    for data in data_rows:
+        print(data)
