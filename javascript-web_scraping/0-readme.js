770f5af3bc2674d675846fb1d6288922694f43eb
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-21 17:04:22 -0500

    Readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, (error, data) => {
+  if (error) console.log(error);
+  if (!error) console.log(data.toString());
+});
