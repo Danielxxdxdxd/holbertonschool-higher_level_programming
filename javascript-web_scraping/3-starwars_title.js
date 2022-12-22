50a710a37083acadbc3b60b0ac10982bb5f2ad77
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-22 15:52:13 -0500

    Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const request = require('request');
+
+request.get(url, (error, response, data) => {
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(data).title);
+  }
+});
