69ee89bdb0ae6e1d21dc49d57775a28fb5583057
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-22 19:50:05 -0500

    Loripsum

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const file = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+
+request.get(url, (error, response, data) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(file, data, (error) => {
+      if (error) console.log(error);
+    });
+  }
+});
