be12d08a8c90646cffbf17acb9d5f8040f9808c1
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-23 13:58:48 -0500

    finish solution

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let counter = 0;
 
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  const bodyObj = JSON.parse(body).results;
+  for (let i = 0; i < bodyObj.length; i++) {
+    const charactersList = bodyObj[i].characters;
+    for (let j = 0; j < charactersList.length; j++) {
+      const characterID = charactersList[j].split('/')[5];
+      if (characterID === '18') {
+        counter += 1;
+      }
+    }
+  }
+
+  console.log(counter);
+});
